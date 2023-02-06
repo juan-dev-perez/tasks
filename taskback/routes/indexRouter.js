3e9e762c026d19b6556fa995de5916f0248b42af
Juan David Perez <juandevperez@gmail.com>
2023-02-06 01:41:09 -0300

    old files deleted

--- taskback/routes/indexRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const tasksController = require('../controllers/tasksController');
-
-router.get('/',tasksController.getTask );
-
-module.exports = router;
