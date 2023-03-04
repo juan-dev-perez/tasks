18b6cbfa7d53a5dca5d2bc6d433717c6f159a702
Juan David Perez <juandevperez@gmail.com>
2023-03-03 22:12:58 -0300

    change main route

--- Backend/routes/index.Router.js
+++ Backend/routes/index.Router.js
@@ -4,8 +4,8 @@ import tasksRouter from './tasks.Router.js';
 const router = Router();
 
 router.use('/tasks',tasksRouter);
-router.get('*', (req, res) => {
-    res.redirect('/');
-});
+// router.get('*', (req, res) => {
+//     res.redirect('/');
+// });
 
 export default router;
